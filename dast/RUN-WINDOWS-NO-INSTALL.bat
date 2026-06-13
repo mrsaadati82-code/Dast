e89e3948f01b88b9866ea2543f8e4e8b291f16b9
mrsaadati82-code <mrsaadati82@gmail.com>
2026-06-13 16:38:28 +0330

    Delete dast directory

--- dast/RUN-WINDOWS-NO-INSTALL.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting Dast Rast on http://localhost:8787
-echo If the browser does not open automatically, open http://localhost:8787
-start http://localhost:8787
-node server-standalone.js
-pause
